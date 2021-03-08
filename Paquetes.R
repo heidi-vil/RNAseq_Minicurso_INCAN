a83496e5913e50fa03286c4b09c2dfa245bd1376
Erick Navarro <32532987+ErickNavarroD@users.noreply.github.com>
2021-03-08 16:18:04 -0600

    Add files via upload

---
+++ Paquetes.R
@@ -0,0 +1,11 @@
+#Instalacion de paquetes
+install.packages("tidyr")
+install.packages("BiocManager") #Paquete que te permite descargar los de bioconductor
+BiocManager::install("DESeq2") #Para el analisis de expresion diferencial
+BiocManager::install("biomaRt") #Anotacion de genes
+install.packages("ggplot2") #Graficos
+BiocManager::install("EnhancedVolcano") #Volcano plots
+install.packages("pheatmap") #Heatmaps
+BiocManager::install("clusterProfiler") #Gene Set Enrichment Analysis
+BiocManager::install("org.Hs.eg.db") #Anotacion del humano
+install.packages("ggnewscale")
